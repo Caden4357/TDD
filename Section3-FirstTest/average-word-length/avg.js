2c4849b686a1cf985f9ff828cee44b380b19bf0c
Caden Wilcox <wilcox4357@gmail.com>
2024-02-11 01:01:39 -0800

    finished avg word length following TDD practices gonna compare to lectures next

--- Section3-FirstTest/average-word-length/avg.js
+++ Section3-FirstTest/average-word-length/avg.js
@@ -1,5 +1,14 @@
 export const avgWordLength = (sentence) => {
+    const splitString = sentence.split(' ')
     if(sentence.length === 0){
         return 0
     }
+    if(splitString.length === 1){
+        return splitString[0].length
+    }
+    let wordLength = 0
+    for(let word of splitString){
+        wordLength += word.length
+    }
+    return Math.ceil(wordLength/splitString.length)
 }
--- Section3-FirstTest/average-word-length/avg.test.js
+++ Section3-FirstTest/average-word-length/avg.test.js
@@ -4,4 +4,10 @@ describe('average word length', () => {
     it('returns 0 when the string is empty', () => {
         expect(avgWordLength('')).toBe(0)
     })
+    it('returns the length of word if only 1 word is given', () => {
+        expect(avgWordLength('ads')).toBe(3)
+    })
+    it('returns the average word length when more than 1 word is given rounded up', () => {
+        expect(avgWordLength('The cow jumped over the moon')).toBe(4)
+    })
 })
