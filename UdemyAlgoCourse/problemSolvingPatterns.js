c81f2f4dc165398df2c1b89ba39d6af843d5d478
Caden Wilcox <wilcox4357@gmail.com>
2024-02-14 19:30:54 -0800

    anagram warmup

--- UdemyAlgoCourse/problemSolvingPatterns.js
+++ UdemyAlgoCourse/problemSolvingPatterns.js
@@ -14,3 +14,32 @@ const frequencyCounter = (arr1, arr2) => {
     console.log(frequency, frequency2);
 }
 console.log(frequencyCounter([1,2,3,4,5], [1,2,3,5,2]));
+
+// Anagram 
+const validAnagram = (str1, str2) => {
+    if(str1.length !== str2.length) {
+        return false
+    }
+    const frequencyOfString1 = {}
+
+    for (let char of str1){
+        frequencyOfString1[char] = (frequencyOfString1[char] || 0) + 1
+    }
+    for (let char of str2){
+        if(!frequencyOfString1[char]){
+            return false
+        }
+        frequencyOfString1[char] -= 1
+    }
+    return true
+} 
+console.log(validAnagram('', '')); // true
+console.log(validAnagram('aaz', 'zza')); // false
+console.log(validAnagram('anagram', 'nagaram')); // true
+console.log(validAnagram('rat', 'car')); // false
+console.log(validAnagram('qwerty', 'qeywrt')); // true
+console.log(validAnagram('texttwisttime', 'timetwisttext')); // true
+
+
+
+
--- UdemyAlgoCourse/same_refactored_solution.js
+++ UdemyAlgoCourse/same_refactored_solution.js
@@ -1,10 +1,15 @@
+// In this problem we are looking at 2 arrays and seeing if the second array contains squared numbers from the first array for example 
+// [1,2,3,2,5] & [9,1,4,4,25] is a match and returns true but [1,2,3,2,5], [9,1,4,4,11] does not 
 function same(arr1, arr2){
+    // arrays have to be the same length for this to work
     if(arr1.length !== arr2.length){
         return false;
     }
+    // create 2 objects to show the numbers and how often they appear
     let frequencyCounter1 = {}
     let frequencyCounter2 = {}
     for(let val of arr1){
+        // short hand for create the key val pair at 1 or increase by one 
         frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
     }
     for(let val of arr2){
@@ -14,9 +19,11 @@ function same(arr1, arr2){
     console.log(frequencyCounter2);
     for(let key in frequencyCounter1){
         console.log(frequencyCounter2[key ** 2]);
+        // Check key against FC2
         if(!(key ** 2 in frequencyCounter2)){
             return false
         }
+        // Check value against FC2
         if(frequencyCounter2[key ** 2] !== frequencyCounter1[key]){
             return false
         }
