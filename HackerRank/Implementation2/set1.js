3f94ed59ac4901aa755e83524f706f7fedfba8f0
Caden Wilcox <wilcox4357@gmail.com>
2024-03-15 13:24:42 -0700

    2 hackerrank problems

--- HackerRank/Implementation2/set1.js
+++ HackerRank/Implementation2/set1.js
@@ -11,3 +11,36 @@ export function designerPDFViewer(h, word) {
     }
     return tallest * word.length;
 }
+
+// The Utopian Tree goes through 2 cycles of growth every year. Each spring, it doubles in height. Each summer, its height increases by 1 meter.
+
+// A Utopian Tree sapling with a height of 1 meter is planted at the onset of spring. How tall will the tree be after n growth cycles?
+export function utopiaTreeGrowth(n){
+    let height = 1 
+    for(let num = 1; num <= n; num++){
+        if(num % 2 === 0){
+            height += 1
+        }
+        else{
+            height += height
+        }
+    }
+    return height;
+}
+
+// https://www.hackerrank.com/challenges/angry-professor/problem?isFullScreen=true
+
+// A Discrete Mathematics professor has a class of students. Frustrated with their lack of discipline, the professor decides to cancel class if fewer than some number of students are present when class starts. Arrival times go from on time () to arrived late ().
+
+// Given the arrival time of each student and a threshhold number of attendees, determine if the class is cancelled.
+export function angryProfessor(k, a) {
+    let onTimeOrEarlyCount = 0;
+    for (let arrivalTime of a) {
+        if (arrivalTime <= 0) {
+            onTimeOrEarlyCount++;
+        }
+    }
+    console.log(onTimeOrEarlyCount);
+    console.log(k);
+    return onTimeOrEarlyCount < k ? 'YES' : 'NO';
+}
--- HackerRank/Implementation2/set1.test.js
+++ HackerRank/Implementation2/set1.test.js
@@ -1,4 +1,4 @@
-import { designerPDFViewer } from "./set1";
+import { designerPDFViewer, utopiaTreeGrowth, angryProfessor } from "./set1";
 describe('Hello', () => {
     // it('returns the height of the tallest letter in the string', () => {
     //     expect(designerPDFViewer([1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5], 'abc')).toBe(3);
@@ -7,3 +7,19 @@ describe('Hello', () => {
         expect(designerPDFViewer([1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5], 'abc')).toBe(9);
     })
 });
+
+describe('Utopia tree growth', () => {
+    it('returns the growth of the tree after n cycles each spring (odd numbers) it doubles in height. Each summer (even numbers) it increases by 1', () => {
+        expect(utopiaTreeGrowth(5)).toBe(14)
+        expect(utopiaTreeGrowth(4)).toBe(7)
+        expect(utopiaTreeGrowth(0)).toBe(1)
+
+    })
+})
+describe('Angry Professor', () => {
+    it('returns YES when there as many or more positive numbers in the array as k', () => {
+        expect(angryProfessor(3,[-2,-1,0,1,2])).toBe('NO')
+        expect(angryProfessor(2,[0,-1,2,1])).toBe('NO')
+        expect(angryProfessor(4,[-93, -86, 49, -62, -90, -63, 40, 72, 11, 67])).toBe('NO')
+    })
+})
