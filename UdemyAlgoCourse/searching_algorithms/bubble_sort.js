3846dfb0b53365cad795a23e2abca7a0b66fddd2
Caden Wilcox <wilcox4357@gmail.com>
2024-03-27 15:30:33 -0700

    optimized solution slightly

--- UdemyAlgoCourse/searching_algorithms/bubble_sort.js
+++ UdemyAlgoCourse/searching_algorithms/bubble_sort.js
@@ -28,9 +28,11 @@ function bubbleSortOptimized(arr) {
     let noSwaps;
     for (let i = arr.length; i > 0; i--){
         noSwaps = true;
-        for(let j = 1; j < i; j++){
-            if(arr[j] < arr[j - 1]){
-                swap(arr, j, j - 1);
+        for(let j = 0; j < i - 1; j++){
+            if(arr[j] > arr[j + 1]){
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
                 noSwaps = false;
             }
         }
